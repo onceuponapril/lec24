d98364f2499704df6c61340aaced7bb586f18183
onceuponapril <31635476+onceuponapril@users.noreply.github.com>
2018-04-10 14:28:48 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import model
 
 app = Flask(__name__)
@@ -13,9 +13,41 @@ def index():
         </ul>
     '''
 
-@app.route('/bball')
+@app.route('/bball', methods=['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+
+    return render_template("seasons.html", seasons=seasons)
+
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method=='POST':
+        firstname=request.form['firstname']
+        lastname=request.form['lastname']
+    else:
+        firstname=''
+        lastname=''
+
+    return render_template("hello.html",firstname=firstname,lastname=lastname)
+
+@app.route('/football', methods=['GET', 'POST'])
+def football():
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_fball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_fball_seasons()
+
+    return render_template("seasons.html", seasons=seasons)
 
 if __name__ == '__main__':
+    model.init_bball()
     app.run(debug=True)
